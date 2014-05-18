df616c5cd898acba65d23e5271fc2ee50ceeb331
Cristian-Valcea <valcea.cristian@gmail.com>
2014-05-18 22:50:11 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beacon::Application.config.secret_token = '926982c25ffc8a9ea5842d61a1c90b49af8df4eb526c2e18aa53af54f91ea5e7040d51952a62efc3937d820fc78273791a45828f530ceec0bcc503bb2a0d7ad4'
