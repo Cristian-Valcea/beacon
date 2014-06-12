5c90c75d0125093d3450b263c02663801a0cfffa
Cristian-Valcea <valcea.cristian@gmail.com>
2014-06-12 23:35:59 +0200

    delete beacons

--- app/controllers/ibeacons_controller.rb
+++ app/controllers/ibeacons_controller.rb
@@ -1,5 +1,6 @@
 class IbeaconsController < ApplicationController 
 	before_filter :signed_in_user, only: [:create, :destroy]
+	before_filter :correct_user,	only: :destroy
 	
 	def create
 	@ibeacon = current_user.ibeacons.build(params[:ibeacon]) 
@@ -10,7 +11,15 @@ class IbeaconsController < ApplicationController
 		render 'static_pages/home' end
 	end
 	
-	def destroy
+	def destroy 
+		@ibeacon.destroy
+		redirect_back_or root_path
 	end
-	 
+	
+	def correct_user
+		@ibeacon = current_user.ibeacons.find_by_id(params[:id])
+		redirect_to root_path if @ibeacon.nil?
+	end	
+	
+		 
 end
